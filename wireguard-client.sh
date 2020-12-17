c8613a20addd7ef44724eea44161f9927d327074
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-12-17 12:44:00 -0500

    Update wireguard-client.sh

--- wireguard-client.sh
+++ wireguard-client.sh
@@ -44,13 +44,11 @@ dist-check
 
 # Pre-Checks
 function installing-system-requirements() {
-  if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ];} then
+  if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ]; }; then
     apt-get update && apt-get install iptables curl bc -y
-  fi
-  if { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ];} then
+  elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
     yum update -y && yum install epel-release iptables curl bc -y
-  fi
-  if { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ];} then
+  elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
     pacman -Syu --noconfirm iptables curl bc
   fi
 }
@@ -101,23 +99,19 @@ if [ ! -f "$WG_CONFIG" ]; then
     KERNEL_VERSION_LIMIT=5.6
     KERNEL_CURRENT_VERSION=$(uname -r | cut -c1-3)
     if (($(echo "$KERNEL_CURRENT_VERSION <= $KERNEL_VERSION_LIMIT" | bc -l))); then
-      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ];} then
+      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ]; }; then
         apt-get update
         apt-get install linux-headers-"$(uname -r)" -y
-      fi
-      if [ "$DISTRO" == "raspbian" ]; then
+      elif [ "$DISTRO" == "raspbian" ]; then
         apt-get update
         apt-get install raspberrypi-kernel-headers -y
-      fi
-      if { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ];} then
+      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
         pacman -Syu
         pacman -Syu --noconfirm linux-headers
-      fi
-      if [ "$DISTRO" == "fedora" ]; then
+      elif [ "$DISTRO" == "fedora" ]; then
         dnf update -y
         dnf install kernel-headers-"$(uname -r)" kernel-devel-"$(uname -r)" -y
-      fi
-      if { [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ];} then
+      elif { [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
         yum update -y
         yum install kernel-headers-"$(uname -r)" kernel-devel-"$(uname -r)" -y
       fi
@@ -129,89 +123,78 @@ if [ ! -f "$WG_CONFIG" ]; then
   # Kernel Version
   install-kernel-headers
 
-# Install WireGuard Client
-function install-wireguard-client() {
-  # Installation begins here.
-  if [ "$DISTRO" == "ubuntu" ] && { [ "$DISTRO_VERSION" == "20.10" ] || [ "$DISTRO_VERSION" == "20.04" ] || [ "$DISTRO_VERSION" == "19.10" ];} then
-    apt-get update
-    apt-get install wireguard qrencode haveged resolvconf -y
-  fi
-  if [ "$DISTRO" == "ubuntu" ] && { [ "$DISTRO_VERSION" == "16.04" ] || [ "$DISTRO_VERSION" == "18.04" ];} then
-    apt-get update
-    apt-get install software-properties-common -y
-    add-apt-repository ppa:wireguard/wireguard -y
-    apt-get update
-    apt-get install wireguard qrencode haveged resolvconf -y
-  fi
-  if [ "$DISTRO" == "debian" ]; then
-    apt-get update
-    echo "deb http://deb.debian.org/debian/ unstable main" >/etc/apt/sources.list.d/unstable.list
-    printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' >/etc/apt/preferences.d/limit-unstable
-    apt-get update
-    apt-get install wireguard qrencode haveged resolvconf -y
-  fi
-  if [ "$DISTRO" == "pop" ]; then
-    apt-get update
-    apt-get install wireguard qrencode haveged ifupdown -y
-  fi
-  if [ "$DISTRO" == "raspbian" ]; then
-    apt-get update
-    apt-get install dirmngr -y
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC
-    echo "deb http://deb.debian.org/debian/ unstable main" >/etc/apt/sources.list.d/unstable.list
-    printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' >/etc/apt/preferences.d/limit-unstable
-    apt-get update
-    apt-get install wireguard qrencode haveged resolvconf -y
-  fi
-  if { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ];} then
-    pacman -Syu
-    pacman -Syu --noconfirm haveged qrencode iptables
-    pacman -Syu --noconfirm wireguard-tools wireguard-arch resolvconf
-  fi
-  if [ "$DISTRO" = "fedora" ] && [ "$DISTRO_VERSION" == "32" ]; then
-    dnf update -y
-    dnf install qrencode wireguard-tools haveged resolvconf -y
-  fi
-  if [ "$DISTRO" = "fedora" ] && { [ "$DISTRO_VERSION" == "30" ] || [ "$DISTRO_VERSION" == "31" ];} then
-    dnf update -y
-    dnf copr enable jdoss/wireguard -y
-    dnf install qrencode wireguard-dkms wireguard-tools haveged resolvconf -y
-  fi
-  if [ "$DISTRO" == "centos" ] && [ "$DISTRO_VERSION" == "8" ]; then
-    yum update -y
-    yum install resolvconf -y
-    yum config-manager --set-enabled PowerTools
-    yum copr enable jdoss/wireguard -y
-    yum install wireguard-dkms wireguard-tools qrencode haveged -y
-  fi
-  if [ "$DISTRO" == "centos" ] && [ "$DISTRO_VERSION" == "7" ]; then
-    yum update -y
-    curl https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo --create-dirs -o /etc/yum.repos.d/wireguard.repo
-    yum update -y
-    yum install wireguard-dkms wireguard-tools qrencode haveged resolvconf -y
-  fi
-  if [ "$DISTRO" == "rhel" ] && [ "$DISTRO_VERSION" == "8" ]; then
-    yum update -y
-    yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-    yum update -y
-    subscription-manager repos --enable codeready-builder-for-rhel-8-"$(arch)"-rpms
-    yum copr enable jdoss/wireguard
-    yum install wireguard-dkms wireguard-tools qrencode haveged resolvconf -y
-  fi
-  if [ "$DISTRO" == "rhel" ] && [ "$DISTRO_VERSION" == "7" ]; then
-    yum update -y
-    curl https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo --create-dirs -o /etc/yum.repos.d/wireguard.repo
-    yum update -y
-    yum install wireguard-dkms wireguard-tools qrencode haveged resolvconf -y
-  fi
-}
+  # Install WireGuard Client
+  function install-wireguard-client() {
+    # Installation begins here.
+    if [ "$DISTRO" == "ubuntu" ] && { [ "$DISTRO_VERSION" == "20.10" ] || [ "$DISTRO_VERSION" == "20.04" ] || [ "$DISTRO_VERSION" == "19.10" ]; }; then
+      apt-get update
+      apt-get install wireguard qrencode haveged resolvconf -y
+    elif [ "$DISTRO" == "ubuntu" ] && { [ "$DISTRO_VERSION" == "16.04" ] || [ "$DISTRO_VERSION" == "18.04" ]; }; then
+      apt-get update
+      apt-get install software-properties-common -y
+      add-apt-repository ppa:wireguard/wireguard -y
+      apt-get update
+      apt-get install wireguard qrencode haveged resolvconf -y
+    elif [ "$DISTRO" == "debian" ]; then
+      apt-get update
+      echo "deb http://deb.debian.org/debian/ unstable main" >/etc/apt/sources.list.d/unstable.list
+      printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' >/etc/apt/preferences.d/limit-unstable
+      apt-get update
+      apt-get install wireguard qrencode haveged resolvconf -y
+    elif [ "$DISTRO" == "pop" ]; then
+      apt-get update
+      apt-get install wireguard qrencode haveged ifupdown -y
+    elif [ "$DISTRO" == "raspbian" ]; then
+      apt-get update
+      apt-get install dirmngr -y
+      apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC
+      echo "deb http://deb.debian.org/debian/ unstable main" >/etc/apt/sources.list.d/unstable.list
+      printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' >/etc/apt/preferences.d/limit-unstable
+      apt-get update
+      apt-get install wireguard qrencode haveged resolvconf -y
+    elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+      pacman -Syu
+      pacman -Syu --noconfirm haveged qrencode iptables
+      pacman -Syu --noconfirm wireguard-tools wireguard-arch resolvconf
+    elif [ "$DISTRO" = "fedora" ] && [ "$DISTRO_VERSION" == "32" ]; then
+      dnf update -y
+      dnf install qrencode wireguard-tools haveged resolvconf -y
+    elif [ "$DISTRO" = "fedora" ] && { [ "$DISTRO_VERSION" == "30" ] || [ "$DISTRO_VERSION" == "31" ]; }; then
+      dnf update -y
+      dnf copr enable jdoss/wireguard -y
+      dnf install qrencode wireguard-dkms wireguard-tools haveged resolvconf -y
+    elif [ "$DISTRO" == "centos" ] && [ "$DISTRO_VERSION" == "8" ]; then
+      yum update -y
+      yum install resolvconf -y
+      yum config-manager --set-enabled PowerTools
+      yum copr enable jdoss/wireguard -y
+      yum install wireguard-dkms wireguard-tools qrencode haveged -y
+    elif [ "$DISTRO" == "centos" ] && [ "$DISTRO_VERSION" == "7" ]; then
+      yum update -y
+      curl https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo --create-dirs -o /etc/yum.repos.d/wireguard.repo
+      yum update -y
+      yum install wireguard-dkms wireguard-tools qrencode haveged resolvconf -y
+    elif [ "$DISTRO" == "rhel" ] && [ "$DISTRO_VERSION" == "8" ]; then
+      yum update -y
+      yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+      yum update -y
+      subscription-manager repos --enable codeready-builder-for-rhel-8-"$(arch)"-rpms
+      yum copr enable jdoss/wireguard
+      yum install wireguard-dkms wireguard-tools qrencode haveged resolvconf -y
+    elif [ "$DISTRO" == "rhel" ] && [ "$DISTRO_VERSION" == "7" ]; then
+      yum update -y
+      curl https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo --create-dirs -o /etc/yum.repos.d/wireguard.repo
+      yum update -y
+      yum install wireguard-dkms wireguard-tools qrencode haveged resolvconf -y
+    fi
+  }
 
-# WireGuard Client
-install-wireguard-client
+  # WireGuard Client
+  install-wireguard-client
 
-echo "Move the files to /etc/wireguard/$WIREGUARD_PUB_NIC.conf"
+  echo "Move the files to /etc/wireguard/$WIREGUARD_PUB_NIC.conf"
 
-echo "if pgrep systemd-journal; then
+  echo "if pgrep systemd-journal; then
       systemctl enable wg-quick@$WIREGUARD_PUB_NIC
       systemctl restart wg-quick@$WIREGUARD_PUB_NIC
     else
@@ -221,8 +204,8 @@ echo "if pgrep systemd-journal; then
 
 else
 
-# take user input
-function take-user-input() {
+  # take user input
+  function take-user-input() {
     echo "What do you want to do?"
     echo "   1) Show WireGuard Interface"
     echo "   2) Start WireGuard Interface"
@@ -236,7 +219,7 @@ function take-user-input() {
     done
     case $USER_OPTIONS in
     1)
-        wg show
+      wg show
       ;;
     2)
       if pgrep systemd-journal; then
@@ -264,16 +247,14 @@ function take-user-input() {
       fi
       ;;
     5)
-      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ];} then
+      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ]; }; then
         dpkg-reconfigure wireguard-dkms
         modprobe wireguard
         systemctl restart wg-quick@$WIREGUARD_PUB_NIC
-      fi
-      if { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ];} then
+      elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
         yum reinstall wireguard-dkms -y
         service wg-quick@$WIREGUARD_PUB_NIC restart
-      fi
-      if { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ];} then
+      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
         pacman -Rs --noconfirm wireguard-tools
         service wg-quick@$WIREGUARD_PUB_NIC restart
       fi
@@ -310,7 +291,7 @@ function take-user-input() {
           apt-get remove --purge wireguard qrencode haveged dirmngr -y
           rm -f /etc/apt/sources.list.d/unstable.list
           rm -f /etc/apt/preferences.d/limit-unstable
-        elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ];} then
+        elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
           pacman -Rs wireguard qrencode haveged -y
         elif [ "$DISTRO" == "fedora" ]; then
           dnf remove wireguard qrencode haveged unbound -y
@@ -331,9 +312,9 @@ function take-user-input() {
       chmod +x "$CURRENT_FILE_PATH" || exit
       ;;
     esac
-}
+  }
 
-# run the function
-take-user-input
+  # run the function
+  take-user-input
 
 fi
