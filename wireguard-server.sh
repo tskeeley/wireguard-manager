68fce5d5a36d801b3cfb696cab23919c533a9a3c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-12-17 12:12:42 -0500

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://github.com/complexorganizations/wireguard-manager
 
-# Require script to be run as root (or with sudo)
+# Require script to be run as root
 function super-user-check() {
   if [ "$EUID" -ne 0 ]; then
     echo "You need to run this script as super user."
