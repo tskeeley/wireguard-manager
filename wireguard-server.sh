e60472c3b07f82181cd1762240a99726a1b59c47
Prajwal Koirala <prajwalkoirala23@protonmail.com>
2020-12-17 05:48:28 -0500

    update

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -95,7 +95,7 @@ kernel-check
 
 function usage-guide() {
   # shellcheck disable=SC2233
-  echo 'usage: ./"$(basename $0)" [options]'
+  echo "usage: ./"$(basename $0)" [options]"
   echo "  --install     Install WireGuard Interface"
   echo "  --start       Start WireGuard Interface"
   echo "  --stop        Stop WireGuard Interface"
