269ee9addc89196a25acf2551addb43a4f12f029
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-22 23:45:26 -0500

    Update wireguard-client.sh

--- wireguard-client.sh
+++ wireguard-client.sh
@@ -346,7 +346,7 @@ else
       ;;
     7) # Update the script
       CURRENT_FILE_PATH="$(realpath "$0")"
-      if [ -f $CURRENT_FILE_PATH ]; then
+      if [ -f "$CURRENT_FILE_PATH" ]; then
         curl -o "$CURRENT_FILE_PATH" https://raw.githubusercontent.com/complexorganizations/wireguard-manager/main/wireguard-server.sh
         chmod +x "$CURRENT_FILE_PATH" || exit
       fi
