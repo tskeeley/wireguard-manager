c04cc0840378ca11e22e413feaad4c5223465f35
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-24 23:46:42 -0500

    Update wireguard-server.sh

--- wireguard-server.sh
+++ wireguard-server.sh
@@ -306,7 +306,7 @@ if [ ! -f "$WG_CONFIG" ]; then
       SERVER_HOST_V4="$(curl -4 -s 'https://api.ipengine.dev' | jq -r '.network.ip')"
       ;;
     2)
-      SERVER_HOST_V4=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+      SERVER_HOST_V4="$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)"
       ;;
     3)
       read -rp "Custom IPv4: " -e -i "$(curl -4 -s 'https://api.ipengine.dev' | jq -r '.network.ip')" SERVER_HOST_V4
@@ -331,7 +331,7 @@ if [ ! -f "$WG_CONFIG" ]; then
       SERVER_HOST_V6="$(curl -6 -s 'https://api.ipengine.dev' | jq -r '.network.ip')"
       ;;
     2)
-      SERVER_HOST_V6=$(ip r get to 2001:4860:4860::8888 | perl -ne '/src { [\w:]+)/ && print "$1\n"')
+      SERVER_HOST_V6="$(ip -6 addr | sed -ne 's|^.* inet6 \([^/]*\)/.* scope global.*$|\1|p' | head -1)"
       ;;
     3)
       read -rp "Custom IPv6: " -e -i "$(curl -6 -s 'https://api.ipengine.dev' | jq -r '.network.ip')" SERVER_HOST_V6
