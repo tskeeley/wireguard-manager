1b00b2a6d4849dc5d828acca3c59a5f32cbb13d8
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-17 07:49:47 -0500

    Update wireguard-manager.sh

--- wireguard-manager.sh
+++ wireguard-manager.sh
@@ -1118,6 +1118,10 @@ else
         LASTIP6=$(grep "/128" $WIREGUARD_CONFIG | tail -n1 | awk '{print $3}' | cut -d "/" -f 1 | cut -d "." -f 4)
         CLIENT_ADDRESS_V4="${PRIVATE_SUBNET_V4::-4}$((LASTIP4 + 1))"
         CLIENT_ADDRESS_V6="${PRIVATE_SUBNET_V6::-4}$((LASTIP6 + 1))"
+        if [ "$LASTIP4" -ge "255" ]; then
+          echo "Error: You have $LASTIP4 peers the max is 255"
+          exit
+        fi
         echo "# $NEW_CLIENT_NAME start
 [Peer]
 PublicKey = $CLIENT_PUBKEY
