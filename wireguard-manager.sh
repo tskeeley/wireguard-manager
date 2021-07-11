3ddfa076b4054d04657846357110ee10df4866b4
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-07-11 19:54:49 -0400

    Update wireguard-manager.sh

--- wireguard-manager.sh
+++ wireguard-manager.sh
@@ -123,8 +123,8 @@ UNBOUND_ROOT_HINTS="${UNBOUND_ROOT}/root.hints"
 UNBOUND_ANCHOR="/var/lib/unbound/root.key"
 UNBOUND_ROOT_SERVER_CONFIG_URL="https://www.internic.net/domain/named.cache"
 UNBOUND_CONFIG_HOST_URL="https://raw.githubusercontent.com/complexorganizations/content-blocker/main/configs/hosts"
-UNBOUND_CONFIG_HOST="${UNBOUND_ROOT}/unbound.conf.d/host.conf"
-UNBOUND_CONFIG_HOST_TMP="/tmp/host"
+UNBOUND_CONFIG_HOST="${UNBOUND_ROOT}/unbound.conf.d/hosts.conf"
+UNBOUND_CONFIG_HOST_TMP="/tmp/hosts"
 
 # Verify that it is an old installation or another installer
 function previous-wireguard-installation() {
