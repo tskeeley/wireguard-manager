c63d358cc17e218f687e30afbf79338983d36189
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-21 12:15:59 -0400

    Update wireguard-manager.sh

--- wireguard-manager.sh
+++ wireguard-manager.sh
@@ -826,6 +826,9 @@ if [ ! -f "${WIREGUARD_CONFIG}" ]; then
         if [ "${INSTALL_UNBOUND}" = "y" ]; then
           read -rp "Do you want to prevent advertisements, tracking, malware, and phishing using the content-blocker? (y/n):" INSTALL_BLOCK_LIST
         fi
+        if [ -z "${INSTALL_UNBOUND}" ]; then
+          INSTALL_UNBOUND="y"
+        fi
         ;;
       2)
         CUSTOM_DNS="y"
