e6d850ceae21b927ded9059af721f7dcfaefb02e
Salvydas Lukosius <sall@w-ss.io>
2021-09-14 18:42:56 +0100

    Update wireguard-manager.sh

--- wireguard-manager.sh
+++ wireguard-manager.sh
@@ -546,7 +546,7 @@ if [ ! -f "${WIREGUARD_CONFIG}" ]; then
     if [ -f "${WIREGUARD_INTERFACE}" ]; then
       echo "What MTU do you want to use?"
       echo "  1) 1280 (Recommended)"
-      echo "  3) Custom (Advanced)"
+      echo "  2) Custom (Advanced)"
       until [[ "${MTU_CHOICE_SETTINGS}" =~ ^[1-2]$ ]]; do
         read -rp "MTU Choice [1-2]:" -e -i 1 MTU_CHOICE_SETTINGS
       done
