8941aa74f1383e2925f4004336b6e7550f1625d2
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-09-16 08:24:16 -0400

    Update wireguard-manager.sh

--- wireguard-manager.sh
+++ wireguard-manager.sh
@@ -409,15 +409,18 @@ if [ ! -f "${WIREGUARD_CONFIG}" ]; then
       done
       case ${SERVER_HOST_V4_SETTINGS} in
       1)
-        SERVER_HOST_V4="$(curl -4 -s 'https://icanhazip.com')"
+        SERVER_HOST_V4="$(curl -4 -s 'https://api.ipengine.dev' | jq -r '.network.ip')"
         if [ -z "${SERVER_HOST_V4}" ]; then
-          echo "Error: Curl unable to locate your server's public IP address."
+            SERVER_HOST_V4="$(curl -4 -s 'https://checkip.amazonaws.com')"
         fi
         ;;
       2)
         read -rp "Custom IPv4:" SERVER_HOST_V4
         if [ -z "${SERVER_HOST_V4}" ]; then
-          SERVER_HOST_V4="$(curl -4 -s 'https://icanhazip.com')"
+          SERVER_HOST_V4="$(curl -4 -s 'https://api.ipengine.dev' | jq -r '.network.ip')"
+        fi
+        if [ -z "${SERVER_HOST_V4}" ]; then
+            SERVER_HOST_V4="$(curl -4 -s 'https://checkip.amazonaws.com')"
         fi
         ;;
       esac
@@ -438,15 +441,18 @@ if [ ! -f "${WIREGUARD_CONFIG}" ]; then
       done
       case ${SERVER_HOST_V6_SETTINGS} in
       1)
-        SERVER_HOST_V6="$(curl -6 -s 'https://icanhazip.com')"
+        SERVER_HOST_V6="$(curl -6 -s 'https://api.ipengine.dev' | jq -r '.network.ip')"
         if [ -z "${SERVER_HOST_V6}" ]; then
-          echo "Error: Curl unable to locate your server's public IP address."
+          SERVER_HOST_V6="$(curl -6 -s 'https://checkip.amazonaws.com')"
         fi
         ;;
       2)
         read -rp "Custom IPv6:" SERVER_HOST_V6
         if [ -z "${SERVER_HOST_V6}" ]; then
-          SERVER_HOST_V6="$(curl -6 -s 'https://icanhazip.com')"
+          SERVER_HOST_V6="$(curl -6 -s 'https://api.ipengine.dev' | jq -r '.network.ip')"
+        fi
+        if [ -z "${SERVER_HOST_V6}" ]; then
+            SERVER_HOST_V6="$(curl -6 -s 'https://checkip.amazonaws.com')"
         fi
         ;;
       esac
@@ -1642,7 +1648,7 @@ PublicKey = ${SERVER_PUBKEY}" >>${WIREGUARD_CLIENT_PATH}/"${NEW_CLIENT_NAME}"-${
         # Change the IP address of your wireguard interface.
         if [ -f "${WIREGUARD_INTERFACE}" ]; then
           OLD_SERVER_HOST=$(head -n1 ${WIREGUARD_CONFIG} | awk '{print $4}' | awk -F: '{print $1}')
-          NEW_SERVER_HOST="$(curl -4 -s 'https://icanhazip.com')"
+          NEW_SERVER_HOST="$(curl -4 -s 'https://api.ipengine.dev' | jq -r '.network.ip')"
           if [ -z "${NEW_SERVER_HOST}" ]; then
             echo "Error: While attempting to locate your IP address, an error occurred."
           fi
