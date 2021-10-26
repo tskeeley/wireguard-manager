419fa5503b532c99a25221d62c313786ad7193b1
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-10-25 23:23:04 -0400

    Update wireguard-manager.sh

--- wireguard-manager.sh
+++ wireguard-manager.sh
@@ -113,7 +113,6 @@ UNBOUND_ANCHOR="/var/lib/unbound/root.key"
 UNBOUND_ROOT_SERVER_CONFIG_URL="https://www.internic.net/domain/named.cache"
 UNBOUND_CONFIG_HOST_URL="https://raw.githubusercontent.com/complexorganizations/content-blocker/main/assets/hosts"
 UNBOUND_CONFIG_HOST="${UNBOUND_ROOT}/unbound.conf.d/hosts.conf"
-UNBOUND_CONFIG_HOST_TMP="/tmp/hosts"
 
 # Usage Guide
 function usage-guide() {
@@ -945,9 +944,8 @@ if [ ! -f "${WIREGUARD_CONFIG}" ]; then
           echo "Unbound: true" >>${UNBOUND_MANAGER}
           if [[ ${INSTALL_BLOCK_LIST} =~ ^[Yy]$ ]]; then
             echo "include: ${UNBOUND_CONFIG_HOST}" >>${UNBOUND_CONFIG}
-            curl "${UNBOUND_CONFIG_HOST_URL}" -o ${UNBOUND_CONFIG_HOST_TMP}
-            awk '$1' ${UNBOUND_CONFIG_HOST_TMP} | awk '{print "local-zone: \""$1"\" redirect\nlocal-data: \""$1" IN A 0.0.0.0\""}' >>${UNBOUND_CONFIG_HOST}
-            rm -f ${UNBOUND_CONFIG_HOST_TMP}
+            curl "${UNBOUND_CONFIG_HOST_URL}" -o ${UNBOUND_CONFIG_HOST}
+            awk '$1' ${UNBOUND_CONFIG_HOST} | awk '{print "local-zone: \""$1"\" redirect\nlocal-data: \""$1" IN A 0.0.0.0\""}' >${UNBOUND_CONFIG_HOST}
           fi
           # restart unbound
           if pgrep systemd-journal; then
@@ -1329,9 +1327,8 @@ PublicKey = ${SERVER_PUBKEY}" >>${WIREGUARD_CLIENT_PATH}/"${NEW_CLIENT_NAME}"-${
         # The block list should be updated.
         if [ -f "${UNBOUND_CONFIG_HOST}" ]; then
           rm -f ${UNBOUND_CONFIG_HOST}
-          curl "${UNBOUND_CONFIG_HOST_URL}" -o ${UNBOUND_CONFIG_HOST_TMP}
-          awk '$1' ${UNBOUND_CONFIG_HOST_TMP} | awk '{print "local-zone: \""$1"\" redirect\nlocal-data: \""$1" IN A 0.0.0.0\""}' >>${UNBOUND_CONFIG_HOST}
-          rm -f ${UNBOUND_CONFIG_HOST_TMP}
+          curl "${UNBOUND_CONFIG_HOST_URL}" -o ${UNBOUND_CONFIG_HOST}
+          awk '$1' ${UNBOUND_CONFIG_HOST} | awk '{print "local-zone: \""$1"\" redirect\nlocal-data: \""$1" IN A 0.0.0.0\""}' >${UNBOUND_CONFIG_HOST}
         fi
         # Once everything is completed, restart the service.
         if pgrep systemd-journal; then
