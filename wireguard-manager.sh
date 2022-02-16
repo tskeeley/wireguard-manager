1d90ff11f9698b4749348ece5e823a28b1f3ea0c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2022-02-16 12:39:36 -0500

    fixed the bug

--- wireguard-manager.sh
+++ wireguard-manager.sh
@@ -1143,8 +1143,8 @@ else
         LASTIPV6="1"
       fi
       # Look for an unused IP address.
-      FIND_UNUSED_IPV4=$(grep "AllowedIPs" ${WIREGUARD_CONFIG} | awk '{print $3}' | cut -d '/' -f 1 | sort | cut -d '.' -f 4 | awk '{for(i=p+1; i<$1; i++) print i} {p=$1}' | grep -v '1$')
-      FIND_UNUSED_IPV6=$(grep "AllowedIPs" ${WIREGUARD_CONFIG} | awk '{print $3}' | cut -d ',' -f 2 | cut -d '/' -f 1 | sort | cut -d ':' -f 5 | awk '{for(i=p+1; i<$1; i++) print i} {p=$1}' | grep -v '1$')
+      FIND_UNUSED_IPV4=$(grep "AllowedIPs" ${WIREGUARD_CONFIG} | awk '{print $3}' | cut -d '/' -f 1 | cut -d '.' -f 4 | sort -n | awk '{for(i=p+1; i<$1; i++) print i} {p=$1}' | grep -v '1$')
+      FIND_UNUSED_IPV6=$(grep "AllowedIPs" ${WIREGUARD_CONFIG} | awk '{print $3}' | cut -d ',' -f 2 | cut -d '/' -f 1 | cut -d ':' -f 5 | sort -n | awk '{for(i=p+1; i<$1; i++) print i} {p=$1}' | grep -v '1$')
       if { [ -n "${FIND_UNUSED_IPV4}" ] && [ -n "${FIND_UNUSED_IPV6}" ]; }; then
         LASTIPV4=$(echo "${FIND_UNUSED_IPV4}" | head -n 1)
         LASTIPV6=$(echo "${FIND_UNUSED_IPV6}" | head -n 1)
